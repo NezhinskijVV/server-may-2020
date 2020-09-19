c7eafefacfd231fa788f3085233b125830f0baff
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-09-19 17:20:15 +0300

    add utils

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,13 +1,29 @@
-import dao.UserDao;
-import dao.UserDaoImpl;
+
+import lombok.SneakyThrows;
+import services.Server;
 
 public class MyApp {
 
+    @SneakyThrows
     public static void main(String[] args) {
-//        new Server().start();
+        new Server().start();
 
-        UserDao dao = new UserDaoImpl();
-
-        System.out.println("dao.findByName(\"Nastya\") = " + dao.findByName("Nastya"));
     }
 }
+
+//HW:
+//1. Регистрация + клиента (авторизация или регистрация?)
+//2. Доделать оставшиеся
+//3. Интернационализация
+
+//1. Logger (@Log4j) (вначале на своем маленьком проекте)
+//2. Сохранение переписки в файл
+//3. Подгрузка переписки
+
+//БД
+//1. Табличка сообщения
+
+//5. Личные сообщения
+//6. Json (библиотека Gson)
+//7. Security (класс Encryptor)
+//8. Swing (Java FX) прикрутить интерфейс
